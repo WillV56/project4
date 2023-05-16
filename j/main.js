e8c0dbd2a752baf840afc23b5f46a97edcd2847c
will <will.vetter56@gmail.com>
2023-05-16 11:50:57 -0700

    new4

--- j/main.js
+++ j/main.js
@@ -1,26 +1,42 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-
+getData();
+   
+document.getElementById("btn").addEventListener('click', getData);
+}
+function getData() {
     fetch("https://swapi.dev/api/people/?page=2")
-    .then(responce => responce.json())
+    .then(response => response.json())
     .then(people => {
     const randomIndex = Math.floor(Math.random() * people.results.length);
     const myData = people.results[randomIndex].name;
     const contentElement = document.createElement("p");
     contentElement.textContent = myData;
     const peopleElement = document.getElementById("people");
+    peopleElement.innerHTML = '';
     peopleElement.appendChild(contentElement);
-    })
 
-    fetch("https://swapi.dev/api/people/?page=2")
-    .then(responce => responce.json())
-    .then(movies => {
-        const myData2 = movies.results[0].films;
-        const contentElement = document.createElement("p");
-        contentElement.textContent = myData2;
-        const moviesElement = document.getElementById("movies");
-        moviesElement.appendChild(contentElement);
+        const myData2 = people.results[randomIndex].films;
+        myData2.forEach(film => {
+            fetch(film)
+            .then(response => response.json())
+            .then(item => {
+            const myData2 = item.title;
+            const contentElement2 = document.createElement("p");
+            contentElement2.textContent = myData2;
+            const moviesElement = document.getElementById("movies");
+            moviesElement.appendChild(contentElement2);
+        })
+        });
+
+       
+ 
+    
+    
+    
+        
+        
     })
 }
 
