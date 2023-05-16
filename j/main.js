bb0ad81f60a508a675ea633c777182821590dc85
will <will.vetter56@gmail.com>
2023-05-16 11:05:23 -0700

    new3

--- j/main.js
+++ j/main.js
@@ -5,10 +5,22 @@ function init() {
     fetch("https://swapi.dev/api/people/?page=2")
     .then(responce => responce.json())
     .then(people => {
-    const myData = people.results[0].name;
+    const randomIndex = Math.floor(Math.random() * people.results.length);
+    const myData = people.results[randomIndex].name;
     const contentElement = document.createElement("p");
     contentElement.textContent = myData;
     const peopleElement = document.getElementById("people");
     peopleElement.appendChild(contentElement);
     })
+
+    fetch("https://swapi.dev/api/people/?page=2")
+    .then(responce => responce.json())
+    .then(movies => {
+        const myData2 = movies.results[0].films;
+        const contentElement = document.createElement("p");
+        contentElement.textContent = myData2;
+        const moviesElement = document.getElementById("movies");
+        moviesElement.appendChild(contentElement);
+    })
 }
+
